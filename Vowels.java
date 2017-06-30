42c971d9668d58b64a5fc51e7703c98b925431a4
lavanyaganesan <lavanyagp2257@gmail.com>
2017-06-30 17:16:56 +0530

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.lang.*;
 public class Vowels
 {
-public static void main(String args[])
+public static void main(String args[]) throws IOException
 {
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in))
 char a;
